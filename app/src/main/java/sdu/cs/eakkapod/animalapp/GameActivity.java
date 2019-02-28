b068196c024b3396b9c5ba2b51a4fd6c1d06f4a0
Eakkapod <zaeak.123@gmail.com>
2019-02-28 11:33:21 +0700

    Animal App 28/2/2562 ผูกตัวแปล

--- app/src/main/java/sdu/cs/eakkapod/animalapp/GameActivity.java
+++ app/src/main/java/sdu/cs/eakkapod/animalapp/GameActivity.java
@@ -2,12 +2,30 @@ package sdu.cs.eakkapod.animalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class GameActivity extends AppCompatActivity {
+
+    //Explicit
+    Button      btn1,btn2,btn3,btn4;
+    ImageView   questionImageView;
+    ImageButton volumeImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-    }
-}
+
+        //ผูกตัวแปรบน Java บน xml
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        questionImageView = findViewById(R.id.imvQuestion);
+        volumeImageButton = findViewById(R.id.imbVolume);
+
+
+    }//End onCreate
+}//End Class
